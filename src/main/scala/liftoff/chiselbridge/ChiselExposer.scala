1fd53890a06b5510e9db50307b6579714f69eac6
Tjark Petersen <tjark-petersen@gmx.de>
2025-12-12 11:54:09 +0100

    ignore chisel internal warning

--- src/main/scala/liftoff/chiselbridge/ChiselExposer.scala
+++ src/main/scala/liftoff/chiselbridge/ChiselExposer.scala
@@ -1,11 +1,14 @@
 package chisel3
 
+import scala.annotation.nowarn
+
 object ChiselExposer {
 
   def enumFactory(enumType: EnumType): ChiselEnum = {
     enumType.factory
   }
 
+  @nowarn("msg=deprecated")
   def topBindingOpt(data: Data): Option[internal.TopBinding] = {
     data.topBindingOpt
   }
